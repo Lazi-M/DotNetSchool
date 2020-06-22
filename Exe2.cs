1e8e9e015610bd7602d973f2eddde6206eae764f
QoomWolf <43828977+QoomWolf@users.noreply.github.com>
2020-06-22 17:49:36 +0200

    Exe2.cs
    Zad. 2. Wyświetl w oknie konsoli cytat zamieszczony w zestawie 0. Wykorzystaj w tym celu metodę Console.WriteLine(). Pamiętaj o zachowaniu rozmieszczenia wersów jeden pod drugim.

---
+++ Exe2.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DotNetSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("  „Czemu ty się, zła godzino,\nz niepotrzebnym mieszasz lękiem ?\n  Jesteś - a więc musisz minąć." +
+                "\nMiniesz - a więc to jest piękne.”\n\n                -Wisława Szymborska");
+           
+            /*zastosowałem spacje i taby by zrobić wcięcia, by wyglądało jak na zdjęciu, 
+            * nie wiem czy to konieczne.
+            */
+        }
+    }
+}
