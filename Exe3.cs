71b948a5045f22a47b635781b87842844c92ede6
QoomWolf <43828977+QoomWolf@users.noreply.github.com>
2020-06-22 17:51:02 +0200

    Exe3.cs
    Zad. 3. Wyświetl ten sam cytat, co w Zad. 2., tym razem korzystając tylko z metody Console.Write(). Pamiętaj o rozmieszczeniu wersów!

---
+++ Exe3.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace DotNetSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("  „Czemu ty się, zła godzino,\nz niepotrzebnym mieszasz lękiem ?\n  Jesteś - a więc musisz minąć." +
+                "\nMiniesz - a więc to jest piękne.”\n\n                -Wisława Szymborska");
+           
+            /*
+            * Wyszo totalnie tak samo, więc coś źle robie :C
+            */
+        }
+    }
+}
